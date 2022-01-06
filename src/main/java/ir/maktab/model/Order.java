63a780a802841ad8b7de5ca5d05acef6e8a9ecf4
ali <zahraabiri816@gmail.com>
2022-01-06 20:21:36 +0330

    add models and enums

---
+++ src/main/java/ir/maktab/model/Order.java
@@ -0,0 +1,27 @@
+package ir.maktab.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Long proposedPrice;
+    private String jobDescription;
+    @CreationTimestamp
+    private Date orderRegistrationDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateOfWorkPerformed;
+    private String address;
+    @ManyToOne
+    private Customer customer;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+    @OneToMany
+    private Set<Offer> suggestions;
+}
