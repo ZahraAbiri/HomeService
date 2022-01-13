5c7414dee7d220cc83fb42761b9e65ebf613f026
FX553V <zahraabiri816@gmail.com>
2022-01-13 19:11:15 +0330

    add managerDao and update MainServise Dao

--- src/main/java/ir/maktab/data/MainServiceDao.java
+++ src/main/java/ir/maktab/data/MainServiceDao.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface MainService extends JpaRepository<MainService,Integer> {
-    Optional<MainService> findByName(String name);
+public interface MainServiceDao extends JpaRepository<MainServiceDao,Integer> {
+    Optional<MainServiceDao> findByName(String name);
 
 }
